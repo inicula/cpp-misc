b3683ad63925c860112612d7c16952493db2e708
Nicula Ionut <niculaionut@tutanota.com>
2022-07-07 21:22:19 +0300

    add more threads

--- false_sharing.cpp
+++ false_sharing.cpp
@@ -6,15 +6,15 @@ using u64 = std::uint64_t;
 /* GNU specific */
 #define DO_NOT_OPTIMIZE __attribute__((optimize("O0")))
 
-/* Try with: `alignas(8)` (minimum alignment for std::uint64_t) */
-struct alignas(64) Storage
+/* Try with: `alignas(64)` */
+struct alignas(8) Storage
 {
         u64 val{0};
 };
 
-static Storage results[2] = {};
+static Storage results[4] = {};
 
-void DO_NOT_OPTIMIZE
+static void DO_NOT_OPTIMIZE
 do_stuff(const std::size_t idx)
 {
         auto& ref = results[idx];
@@ -30,9 +30,13 @@ main()
 {
         std::thread t0(do_stuff, 0);
         std::thread t1(do_stuff, 1);
+        std::thread t2(do_stuff, 2);
+        std::thread t3(do_stuff, 3);
 
         t0.join();
         t1.join();
+        t2.join();
+        t3.join();
 
-        std::cout << results[0].val + results[1].val << '\n';
+        std::cout << results[0].val + results[1].val + results[2].val + results[3].val << '\n';
 }
