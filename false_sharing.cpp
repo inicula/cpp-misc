081d725214a1360a5be70c7a7b63cb9cd7b2ee6e
Nicula Ionut <niculaionut@tutanota.com>
2022-07-07 21:03:33 +0300

    add false sharing example

---
+++ false_sharing.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <thread>
+
+using u64 = std::uint64_t;
+
+/* gcc specific */
+#define DO_NOT_OPTIMIZE __attribute__((optimize("O0")))
+
+/* try with: `alignas(8)` (minimum alignment for std::uint64_t) */
+struct alignas(64) Storage
+{
+        u64 val;
+};
+
+static Storage results[2] = {{0}, {0}};
+
+void DO_NOT_OPTIMIZE
+do_stuff(const std::size_t idx)
+{
+        auto& ref = results[idx];
+
+        for(std::size_t i = 0; i < 1 << 26; ++i)
+        {
+                ++ref.val;
+        }
+}
+
+int
+main()
+{
+        std::thread t0(do_stuff, 0);
+        std::thread t1(do_stuff, 1);
+
+        t0.join();
+        t1.join();
+
+        std::cout << results[0].val + results[0].val << '\n';
+}
